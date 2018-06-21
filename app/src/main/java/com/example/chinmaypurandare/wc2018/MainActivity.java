2bfd87ce1b29f25f7dbac6b925cf168588cd2521
CHinmay Purandare <chippurandare@gmail.com>
2018-06-22 03:53:56 +0530

    retry after config

---
+++ app/src/main/java/com/example/chinmaypurandare/wc2018/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.chinmaypurandare.wc2018;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button click;
+    public static TextView match;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        click = findViewById(R.id.button3);
+        match = findViewById(R.id.match);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FetchData process = new FetchData();
+                process.execute();
+
+            }
+        });
+    }
+}
